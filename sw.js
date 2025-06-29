52b22d84a2daea68e026e1053ea0fd0981cb2681
Nurish1907 <mohamadnurish@gmail.com>
2025-06-29 11:12:59 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,24 @@
+const cacheName = "workout-cache-v1";
+const filesToCache = [
+  "index.html",
+  "manifest.json",
+  "sw.js",
+  "icon-192.png",
+  "icon-512.png"
+];
+
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(response => {
+      return response || fetch(e.request);
+    })
+  );
+});
